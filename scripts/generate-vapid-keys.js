9287f0d2d8605656d2fbcdea1036dbd7d5a4932f
EECOLOR <ewestra@kaliber.net>
2025-07-11 15:36:01 +0200

    Add script to generate VAPID keys

---
+++ scripts/generate-vapid-keys.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import crypto from 'node:crypto'
+import fs from 'node:fs'
+import path from 'node:path'
+
+const [executor, currentFile, targetFileName] = process.argv
+
+if (!targetFileName)
+  errorExit(`Missing target file name, usage: generate-vapid-keys vapid-keys.json`)
+
+ensureKeyFile(targetFileName)
+
+function ensureKeyFile(keyFile) {
+  if (fs.statSync(keyFile, { throwIfNoEntry: false })?.isFile())
+    return console.log('Skipping creation of keys: file already exists')
+
+  console.log(`Generating keys...`)
+  const vapidKeys = generateVapidKeys()
+
+  fs.mkdirSync(path.dirname(keyFile), { recursive: true })
+  fs.writeFileSync(keyFile, JSON.stringify(vapidKeys, null, 2))
+  console.log(`Keys stored at ${keyFile}`)
+}
+
+
+function generateVapidKeys() {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync('ec', { namedCurve: 'P-256' })
+
+  const publicKeyBuffer = Buffer.from(
+    publicKey.export({ type: 'spki', format: 'der' }).subarray(26)
+  )
+
+  return {
+    publicKeyBase64Url: publicKeyBuffer.toString('base64url'),
+    privateKeyPem: privateKey.export({ type: 'pkcs8', format: 'pem' }),
+  }
+}
+
+function errorExit(message) {
+  console.error(message)
+  process.exit(1)
+}
